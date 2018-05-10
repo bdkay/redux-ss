db7adf3c9f808aaf16df9de47c3e9ad6a22c99e7
Brendon Kuleck <bkuleck@caxy.com>
2018-05-10 12:42:18 -0500

    Super simple search component

---
+++ src/components/search_bar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// Component for searching videos
+const SearchBar = () => {
+  return <input />; // React.createElement
+};
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar'
+
 const API_KEY = 'AIzaSyDCgD1fx18NbRaSGFHnVJy63ryMhezFO34';
 
 // Create a new component that produces some html
 // const App = function(){}
 const App = () => {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
