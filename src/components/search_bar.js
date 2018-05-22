c17b0adedf549e8f708eb9af499788e07a5fa601
Brendon Kuleck <bkuleck@caxy.com>
2018-05-22 14:39:30 -0500

    Search bar state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 
 // Component for searching videos
 
-// Class-based component
+// Class-based component w/ ES6, extends gives it added functionality
 class SearchBar extends Component {
+  constructor(props){
+    // Initializing state in a class-based component
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   render () {
+ // return <input onChange={(event) => console.log(event.target.value))} />;
+ // Single line arrow functions can drop leading parens
     return <input onChange={(event => console.log(event.target.value))} />;
   }
 
