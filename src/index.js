151390456a8b6394b996485baf1afccf15f38f80
Brendon Kuleck <brendonkuleck@gmail.com>
2018-06-05 14:41:01 -0500

    Access to videos on state

--- src/index.js
+++ src/index.js
@@ -6,13 +6,24 @@ import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyDCgD1fx18NbRaSGFHnVJy63ryMhezFO34';
 
-YTSearch({ key: API_KEY, term: 'surfboards' }, function(data){
-  console.log(data);
-});
-
 // Create a new component that produces some html
 // const App = function(){}
 class App extends Component {
+  //Runs immediately, kicks off search
+  constructor(props){
+    super(props);
+
+    //Array of videos
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      //Callback function to update list with videos returned
+
+      //In ES6, if key and value are the same, you can delete one.
+      this.setState({ videos });
+      //this.setState({ videos: videos })
+    });
+  }
   render(){
     return (
       <div>
