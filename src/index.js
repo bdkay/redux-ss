5ed896415949cc03737271e4bcf325a413d92a85
Brendon Kuleck <brendonkuleck@gmail.com>
2018-06-05 14:25:59 -0500

    App goes from functional to class

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyDCgD1fx18NbRaSGFHnVJy63ryMhezFO34';
 
+YTSearch({ key: API_KEY, term: 'surfboards' }, function(data){
+  console.log(data);
+});
+
 // Create a new component that produces some html
 // const App = function(){}
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
